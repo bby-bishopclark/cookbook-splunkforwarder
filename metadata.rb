78f7421be0c6a5106967712f2e2a46d3c5fe3e4c
Ken Miles <ken.miles@hp.com>
2013-12-07 18:18:12 -0800

    Switching to using platform_family.  Requires Chef Client 10.10+

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
 name    'splunkforwarder'
-version '1.0.1'
+version '2.0.1'
 
-description 'Installs & Configures Splunk Forwarder'
+description 'Installs & Configures Splunk Forwarder to a Linux Client'
 
-maintainer 'Greg Albrecht'
-maintainer_email 'gba@splunk.com'
+maintainer 'Ken Miles'
+maintainer_email 'ken.miles@hp.com'
 license 'Apache License 2.0'
 
 
-%w{redhat centos fedora debian suse ubuntu}.each do |os|
+%w{redhat rhel centos fedora debian suse ubuntu}.each do |os|
   supports os
 end
 
--- recipes/package.rb
+++ recipes/package.rb
@@ -9,9 +9,7 @@
 # License:: Apache License 2.0
 #
 
-
 download_dir = '/usr/src'
-
 
 # e.g. splunkforwarder-1.2.3-123456
 package_prefix = [
@@ -20,27 +18,21 @@ package_prefix = [
   node['splunkforwarder']['build']
 ].join('-')
 
+Chef::Log.info("platform_family = #{node['platform_family']}; \
+  kernel.machine = #{node['kernel']['machine']}")
 
-package_suffix = value_for_platform(
-  ['centos', 'redhat', 'suse', 'fedora'] => {
-    'default' => if node['kernel']['machine'] == 'x86_64'
-      '-linux-2.6-x86_64.rpm'
-    else
-      '.i386.rpm'
-    end
-  },
-  ['debian', 'ubuntu'] => {
-    'default' => if node['kernel']['machine'] == 'x86_64'
-      '-linux-2.6-amd64.deb'
-    else
-      '-linux-2.6-intel.deb'
-    end
-  }
-)
+if platform_family?("rhel", "suse", "fedora")
+  package_suffix = node['kernel']['machine'] == 'x86_64' ? '-linux-2.6-x86_64.rpm' : '.i386.rpm'
+elsif platform_family?("debian")
+  package_suffix = node['kernel']['machine'] == 'x86_64' ? '-linux-2.6-amd64.deb' : '-linux-2.6-intel.deb'
+else
+  raise "Unsupported platform_family: #{node['platform_family']}"
+end
 
-
+# Chef::Log.info("package_suffix = #{package_suffix}")
 package_name = [package_prefix, package_suffix].join
 package_download_path = File.join(download_dir, package_name)
+Chef::Log.info("package_name = #{package_name}")
 
 # e.g http://downloads.splunk.com/5.0/u/l/s-1.2.3-12345.tar.gz
 package_url = [
@@ -51,9 +43,7 @@ package_url = [
   package_name
 ].join('/')
 
-
 directory download_dir
-
 
 remote_file 'splunkforwarder: Download Package' do
   action :nothing
@@ -62,7 +52,6 @@ remote_file 'splunkforwarder: Download Package' do
   path package_download_path
   source package_url
 end
-
 
 http_request 'splunkforwarder: Check Package URL' do
   action :head
@@ -76,16 +65,22 @@ http_request 'splunkforwarder: Check Package URL' do
   url package_url
 end
 
+# package package_name do
+#   options node['splunkforwarder']['install_options']
+#   source package_download_path
+# end
+# As of Chef 11.8, does not install successfully from file without specifiying 
+# the provider directly. So, ...
 
 package package_name do
-  case node['platform']
-  when 'centos', 'redhat', 'fedora'
-    options node['splunkforwarder']['install_options']
+  options node['splunkforwarder']['install_options']
+  case node['platform_family']
+  when "rhel"
     provider Chef::Provider::Package::Rpm
-  when 'debian', 'ubuntu'
-    options node['splunkforwarder']['install_options']
+  when "debian"
     provider Chef::Provider::Package::Dpkg
+  else 
+    raise "Unsupported platform_family: #{node['platform_family']}"
   end
-
   source package_download_path
 end
